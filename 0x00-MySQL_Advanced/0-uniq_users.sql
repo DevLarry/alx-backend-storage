fad55f0908ea66ef798009c3f1db328c3d6e8d2a
DevLarry <olanrewajusamad070@gmail.com>
2023-10-20 02:01:15 +0100

    Unique users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- 3 first students in the Batch
 -- because Batch 3 is the best!
-CREATE TABLE USER (
-    ID INT NOT NULL AUTO_INCREMENT,
-    EMAIL VARCHAR(255),
-    NAME VARCHAR(255),
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255),
+    name VARCHAR(255),
     PRIMARY KEY (ID)
 ) IF NOT EXISTS;
