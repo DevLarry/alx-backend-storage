e439d261c17d0242b5bebb8404f13b4120e0283f
DevLarry <olanrewajusamad070@gmail.com>
2023-11-01 22:47:22 +0100

    insert int into  database

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""Insert document"""
+
+
 def insert_school(mongo_collection, **kwargs):
     """Insert a documnent into the database"""
     return mongo_collection.insert_one(kwargs).inserted_id
